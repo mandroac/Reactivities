fd1afa6905a4fd0a4353ba9704e2d7f7843485bc
Liubomyr Holovatyi <liubomyr.holovatyi@dataart.com>
2022-01-13 00:29:52 +0200

    added routing

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,5 +1,6 @@
 import { observer } from "mobx-react-lite";
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { Button, Item, Label, Segment } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
 
@@ -28,7 +29,7 @@ export default observer(function ActivityList() {
                             </Item.Description>
                             <Item.Extra>
                                 <Button floated="right" content="View" color="blue"
-                                    onClick={() => activityStore.selectActivity(activity.id)}
+                                    as={Link} to={`/activities/${activity.id}`}
                                 />
                                 <Button 
                                     floated="right" content="Delete" color="red" name={activity.id}
